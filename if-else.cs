d79b4e10c61a6a5d42856f0d24441488241b12d4
ridhwan497 <ridubangire@gmail.com>
2023-04-01 08:39:51 +0300

    if-else_statements

--- if-else.cs
+++ if-else.cs
@@ -1,26 +1,35 @@
 using System;
 
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+namespace if_else_program
+{
+	class if_else
+	{
+		static void main(string[] args)
+		{
+			Random random = new Random();
+			int daysUntilExpiration = random.Next(12);
+			int discountPercentage = 0;
 
-if (daysUntilExpiration <= 10)
-{
-	Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-else if (daysUntilExpiration <= 5)
-{
-	Console.WriteLine("Your subscription expires in " + daysUntilExpiration + " days. Renew now and save 10%!");
-}
-else if (daysUntilExpiration == 1)
-{
-	Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
-}
-else if (daysUntilExpiration == 0)
-{
-	Console.WriteLine("Your subscription has expired.");
-}
-else
-{
-	Console.WriteLine("");
+			if (daysUntilExpiration <= 10)
+			{
+				Console.WriteLine("Your subscription will expire soon. Renew now!");
+			}
+			else if (daysUntilExpiration <= 5)
+			{
+				Console.WriteLine("Your subscription expires in " + daysUntilExpiration + " days. Renew now and save 10%!");
+			}
+			else if (daysUntilExpiration == 1)
+			{
+				Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
+			}
+			else if (daysUntilExpiration == 0)
+			{
+				Console.WriteLine("Your subscription has expired.");
+			}
+			else
+			{
+				Console.WriteLine("");
+			}
+		}
+	}
 }
